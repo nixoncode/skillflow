e47186306f6a537ca7277df18c8d204b3fc3e167
Nixon Kosgei <nkosgey6@gmail.com>
2026-01-21 10:13:59 +0300

    initialize Cobra command

--- api/app/skillflow.go
+++ api/app/skillflow.go
@@ -1,14 +1,24 @@
 package app
 
-import "github.com/nixoncode/skillflow/core"
+import (
+	"github.com/nixoncode/skillflow/core"
+	"github.com/spf13/cobra"
+)
 
 var _ core.App = (*SkillFlowApp)(nil)
 
 type SkillFlowApp struct {
+	rootCmd *cobra.Command
 }
 
 func New() *SkillFlowApp {
-	return &SkillFlowApp{}
+	return &SkillFlowApp{
+		rootCmd: &cobra.Command{
+			Use:     "skillflow",
+			Short:   "SkillFlow is a learning platform API",
+			Version: "1.0.0",
+		},
+	}
 }
 
 func (app *SkillFlowApp) Bootstrap() error {
